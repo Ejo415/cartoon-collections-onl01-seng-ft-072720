fa476f1bb16ba9acbba246efe65ab88b3acffe71
Eugene O'Neil <ejo7220@gmail.com>
2020-07-30 18:42:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   types = cheese_types
-  array.detect{|types|}
+  array.detect{|types| array(types)}
  
   
 end
